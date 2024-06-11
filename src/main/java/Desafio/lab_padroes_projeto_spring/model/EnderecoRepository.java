bb150bcaf22325e0253fc080758c4d36f71418c6
Robson <robson14br@outlook.com>
2024-06-11 12:09:07 -0300

    ProjetoSpring

---
+++ src/main/java/Desafio/lab_padroes_projeto_spring/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package Desafio.lab_padroes_projeto_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
---
+++ src/main/java/Desafio/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,81 @@
+package Desafio.lab_padroes_projeto_spring.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Desafio.lab_padroes_projeto_spring.model.Cliente;
+import Desafio.lab_padroes_projeto_spring.model.ClienteRepository;
+import Desafio.lab_padroes_projeto_spring.model.Endereco;
+import Desafio.lab_padroes_projeto_spring.model.EnderecoRepository;
+import Desafio.lab_padroes_projeto_spring.service.ClienteService;
+import Desafio.lab_padroes_projeto_spring.service.ViaCepService;
+
+/**
+ * Implementação da <b>Strategy</b> {@link ClienteService}, a qual pode ser
+ * injetada pelo Spring (via {@link Autowired}). Com isso, como essa classe é um
+ * {@link Service}, ela será tratada como um <b>Singleton</b>.
+ * 
+ * @author falvojr
+ */
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+	// Singleton: Injetar os componentes do Spring com @Autowired.
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ViaCepService viaCepService;
+	
+	// Strategy: Implementar os métodos definidos na interface.
+	// Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
+
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		// Buscar todos os Clientes.
+		return clienteRepository.findAll();
+	}
+
+	@Override
+	public Cliente buscarPorId(Long id) {
+		// Buscar Cliente por ID.
+		java.util.Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		salvarClienteComCep(cliente);
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		// Buscar Cliente por ID, caso exista:
+		java.util.Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent()) {
+			salvarClienteComCep(cliente);
+		}
+	}
+
+	@Override
+	public void deletar(Long id) {
+		// Deletar Cliente por ID.
+		clienteRepository.deleteById(id);
+	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		// Verificar se o Endereco do Cliente já existe (pelo CEP).
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			// Caso não exista, integrar com o ViaCEP e persistir o retorno.
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		// Inserir Cliente, vinculando o Endereco (novo ou existente).
+		clienteRepository.save(cliente);
+	}
+
+}
